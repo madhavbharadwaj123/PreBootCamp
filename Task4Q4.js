da4d7c874d94df0734915af663b30e8585c74853
madhavbharadwaj123 <bharadwajmadhav669@gmail.com>
2020-07-03 18:13:54 +0530

    Create Task4Q4.js

---
+++ Task4Q4.js
@@ -0,0 +1,14 @@
+function dataHandling(friends)
+{
+  return friends.join(',')
+}
+
+var friends = [
+“Gabbar”,
+“Rajinikanth”,
+“Mass”,
+“Spiderman”,
+“Jeff”,
+“ET”
+];
+console.log(dataHandling(friends))
