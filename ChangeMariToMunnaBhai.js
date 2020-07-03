e1ab6e74711635bf7c0a06b9e7be05ae542a20df
madhavbharadwaj123 <bharadwajmadhav669@gmail.com>
2020-07-03 20:45:24 +0530

    Create ChangeMariToMunnaBhai.js

---
+++ ChangeMariToMunnaBhai.js
@@ -0,0 +1,18 @@
+let friends = [
+“Mari”,
+“MaryJane”,
+“CaptianAmerica”,
+“Munnabai”,
+“Jeff”,
+“AAK chandran”
+];
+function dataHandling(input){
+for (var i = 0; i < input.length; i++) {
+  if(friends[i] == "Mari")
+  {
+    friends[i] = Munnabai
+  }
+  return friends
+}
+}
+dataHandling(friends);
