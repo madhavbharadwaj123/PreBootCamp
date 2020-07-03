89b21a80ab4200969cd36b99840f93dcf1c9743f
madhavbharadwaj123 <bharadwajmadhav669@gmail.com>
2020-07-03 15:00:59 +0530

    Create Task4Q1.js

---
+++ Task4Q1.js
@@ -0,0 +1,12 @@
+var friends2 = [
+“Gabbar”,
+“Rajinikanth”,
+“Mass”,
+“Spiderman”,
+“Jeff”,
+“ET”
+];
+
+
+
+console.log(freinds2[0],friends2[Math.floor(friends2.length/2)],friends2[freinds2.length-1])
