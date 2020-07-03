9e18d7b3c338edc2c7cfa9f7192c7cbd666a2ec4
madhavbharadwaj123 <bharadwajmadhav669@gmail.com>
2020-07-03 23:12:13 +0530

    Create Session3Task14.js

---
+++ Session3Task14.js
@@ -0,0 +1,28 @@
+fix.html
+
+<!DOCTYPE html>
+<html>
+<body>
+ <script src="scripts.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+script.js
+let message;
+let lock = 0;
+//Dont change any code below this
+if (lock && " " || undefined )
+{
+  message = "Go away";
+}
+else
+{
+ message = "welcome";
+}
+console.log(message);
