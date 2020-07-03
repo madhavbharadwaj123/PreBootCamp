fc63e5110e671cad755fdd5062348ed8ecbf00e9
madhavbharadwaj123 <bharadwajmadhav669@gmail.com>
2020-07-03 20:50:19 +0530

    Create PrintUntilFindCaptainAmerica.js

---
+++ PrintUntilFindCaptainAmerica.js
@@ -0,0 +1,19 @@
+const friends = [
+“Mari”,
+“MaryJane”,
+“CaptianAmerica”,
+“Munnabai”,
+“Jeff”,
+“AAK chandran”
+];
+function dataHandling(input){
+for (var i = 0; i < input.length; i++) {
+  
+  if(input[i] == “CaptianAmerica”)
+  {
+  break
+  }
+  console.log(input[i])
+}
+}
+dataHandling(friends);
