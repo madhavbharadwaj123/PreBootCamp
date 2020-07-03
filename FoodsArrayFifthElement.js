caa30c604f8dc7218d03e7278d5d24200326b30d
madhavbharadwaj123 <bharadwajmadhav669@gmail.com>
2020-07-03 20:43:51 +0530

    Create FoodsArrayFifthElement.js

---
+++ FoodsArrayFifthElement.js
@@ -0,0 +1,2 @@
+let foods=['Manchuria','noodles','babycorn','babycorn65','panner','pannertikka','pannermanchuria','chilli panner','corn65','cornbundipakoda','pakodi','gobi65','idiyappam','nuggets','wedges','panner burger','mushroom','crispy corn','chillicorn','bullets']
+console.log(foods[4])
