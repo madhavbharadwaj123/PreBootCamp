bea57c0c8eda763d282b7a14bb65b2edf39e44eb
madhavbharadwaj123 <bharadwajmadhav669@gmail.com>
2020-07-03 17:56:24 +0530

    Update and rename Task4Q4 to Task4Q4.js

---
+++ Task4Q4.js
@@ -0,0 +1,10 @@
+var friends1 = [
+“Gabbar”,
+“Rajinikanth”,
+“Mass”,
+“Spiderman”,
+“Jeff”,
+“ET”
+];
+for(let i=0;i<friends.length;i++){friends[i] = "Mr."+friends[i]}
+console.log(friends.join(' '))
