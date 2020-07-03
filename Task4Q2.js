acb5a67a04e7a6513313f986b29face47a4e2954
madhavbharadwaj123 <bharadwajmadhav669@gmail.com>
2020-07-03 15:03:20 +0530

    Create Task4Q2.js

---
+++ Task4Q2.js
@@ -0,0 +1,17 @@
+var f = [
+“Mari”,
+“MaryJane”,
+“CaptianAmerica”,
+“Munnabai”,
+“Jeff”,
+“AAK chandran”
+];
+
+function handleData(f)
+{
+  f.push('fed')
+  f = ['othername'].push(f)
+  return f
+}
+
+console.log(handleData(f))
